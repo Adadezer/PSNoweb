164a6b6682e37981fa9b302007466ebe68dd4e74
Adadezer <adadezer@gmail.com>
2023-08-15 10:25:33 -0300

    build: :wrench: bootstrap

--- noweb/src/App.js
+++ noweb/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { Card } from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/ProductCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <Navbar name="Camisa Azul" price="20.00" currency="BRL" />
+      <Card>
+        <h1>Olá, Mundo!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Podemos também escrever parágrafos!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </Card>
+    </>
+
   );
 }
 

85d2d12e124cb6dc926d6590b1ed215559d34035
Adadezer <adadezer@gmail.com>
2023-08-17 14:43:32 -0300

    refactor: :recycle: identação

--- noweb/src/components/Categories.js
+++ noweb/src/components/Categories.js
@@ -9,25 +9,25 @@ export default function Categories() {
       <div className="category-categoriesContainer">
         <h3 className="category-title" style={{ fontWeight: 700 }}>CATEGORIAS</h3>
         <div className="category-categoriesList">
-            {arrayCategories.map((category) => (
-              <div>
-                <h5 className="card-title category-cardTitle" style={{ fontWeight: 700 }}>
-                  {category.name === 'Utensílios Domésticos'
-                    ? (<>UTENSÍLIOS <br /> DOMÉSTICOS</>) : (category.name.toUpperCase())}
-                </h5>
-                <div className="card" style={{ backgroundColor: 'transparent', border: 'transparent' }}>
-                  <div className="card-body category-cardBody">
-                    <img
-                      src={category.image}
-                      className="card-img-top"
-                      style={{ width: '100px', height: '100px', margin: '20px' }}
-                      alt="imagemCategoria"
-                    />
-                  </div>
+          {arrayCategories.map((category) => (
+            <div>
+              <h5 className="card-title category-cardTitle" style={{ fontWeight: 700 }}>
+                {category.name === 'Utensílios Domésticos'
+                  ? (<>UTENSÍLIOS <br /> DOMÉSTICOS</>) : (category.name.toUpperCase())}
+              </h5>
+              <div className="card" style={{ backgroundColor: 'transparent', border: 'transparent' }}>
+                <div className="card-body category-cardBody">
+                  <img
+                    src={category.image}
+                    className="card-img-top"
+                    style={{ width: '100px', height: '100px', margin: '20px' }}
+                    alt="imagemCategoria"
+                  />
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );

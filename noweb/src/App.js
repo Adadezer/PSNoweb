bbbe0fe7b5779532a853234d01d45d199d3625eb
Adadezer <adadezer@gmail.com>
2023-08-18 17:03:29 -0300

    feat: :zap: layout capa mobile

--- noweb/src/App.js
+++ noweb/src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Cape from './components/Cape';
-import Products from './components/Products';
-import Categories from './components/Categories';
-import About from './components/About';
-import Telesales from './components/Telesales';
-import Footer from './components/Footer';
+// import Products from './components/Products';
+// import Categories from './components/Categories';
+// import About from './components/About';
+// import Telesales from './components/Telesales';
+// import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
       <Cape />
-      <Products />
-      <Categories />
-      <About />
-      <Telesales />
-      <Footer />
+      {/* <Products /> */}
+      {/* <Categories /> */}
+      {/* <About /> */}
+      {/* <Telesales /> */}
+      {/* <Footer /> */}
     </>
   );
 }

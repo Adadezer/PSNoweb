3c31a613825ec6800377c738960b239179fdd1de
Adadezer <adadezer@gmail.com>
2023-08-18 10:16:31 -0300

    feat: :sparkles: alinhamento div redes sociais

--- noweb/src/components/Footer.js
+++ noweb/src/components/Footer.js
@@ -3,7 +3,7 @@ import '../styles/Footer.css';
 
 export default function Footer() {
   const slogan = `
-    Venha na elétrica J. Santos e garanta a melhor
+    Venha na Elétrica J. Santos e garanta a melhor
     
     opção para sua casa!
   `;
@@ -15,8 +15,13 @@ export default function Footer() {
           <img src="/img/EletricJSantos2.png" className="footer-logoFooter" alt="logo2" />
           <div className="footer-slogan" dangerouslySetInnerHTML={{ __html: slogan.replace(/\n/g, '<br>') }} />
         </div>
-        <div className="footer-redesSociais">
-
+        <div className="footer-socialMedia">
+          <p className="footer-socialText">NOS SIGA NAS REDES</p>
+          <div className="footer-containerSocialLogos">
+            <img src="/img/logoWhatsapp.png" className="footer-socialLogos" alt="logoWhatsApp" />
+            <img src="/img/logoInstagram.png" className="footer-socialLogos" alt="logoInstagram" />
+            <img src="/img/logoFacebook.png" className="footer-socialLogos" alt="logoFacebook" />
+          </div>
         </div>
       </div>
     </div>
